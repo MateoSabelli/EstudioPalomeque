6b238e02e994ccd97c1d11135687b871ae686d99
Mateo Sabelli <msabelli@uade.edu.ar>
2024-05-29 20:05:15 -0300

    Agregue info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Info from './pages/Info/Info'
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <Info/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,67 +4,6 @@ import LogoWeb from '../assets/img/image.png'
 import LogoMob from '../assets/img/logo.png'
 
 const Navbar = () => {
-    /* const [Navbar, setNavbar] = useState(false)
-
-    if(Navbar === true){
-        document.getElementbyId("mobile-menu").classList.remove('hidden')
-        console.log("abrirse")
-    }
-
-
-  return (
-    <div className='h-[10vh] flex justify-between items-center max-w-[1250px] m-auto relative'>
-        <div>
-           <img src={LogoWeb} alt="Logo" className='h-[50px] max-sm:hidden' />
-           <img src={LogoMob} alt="Logo" className='h-[50px] min-[768px]:hidden'/>
-        </div>
-
-        <div>
-            <button onClick={()=> setNavbar(!Navbar)} className='max-sm:block min-[768px]:hidden'>
-                <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6 text-[#C4A66B]' fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </button>
-            <button className='min-[768px]:hidden'>
-                <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6 text-[#C4A66B]' fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-
-
-        <div className=' w-full h-[80vh] flex flex-col justify-center items-center min-[768px]:hidden ' id='mobile-menu'>
-            <nav>
-                <ul className=' text-[#C4A66B] text-base'>
-                    <li>
-                        <Link to='/' className=' p-6'>Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to='/about' className=' p-6'>Areas de practica</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact' className=' p-6'>Contacto</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        
-        
-        <nav className='max-sm:hidden' id='navMobile' >
-            <ul className='flex justify-between text-[#C4A66B] text-base'>
-                <li>
-                    <Link to='/' className=' p-6'>Inicio</Link>
-                </li>
-                <li>
-                    <Link to='/about' className=' p-6'>Areas de practica</Link>
-                </li>
-                <li>
-                    <Link to='/contact' className=' p-6'>Contacto</Link>
-                </li>
-            </ul>
-        </nav>
-    </div>
-  ) */
   
   const [button, setButton] =useState(true)
 
@@ -73,13 +12,11 @@ const Navbar = () => {
   };
 
 const [servicesDropdownOpen, setServicesDropdownOpen] = useState(false);
-const [productsDropdownOpen, setProductsDropdownOpen] = useState(false);
 
 const toggleServicesDropdown = () => setServicesDropdownOpen(!servicesDropdownOpen);
-const toggleProductsDropdown = () => setProductsDropdownOpen(!productsDropdownOpen);
 
   return (
-    <div className='fixed w-full '>
+    <div className=' w-full'>
           <div className='h-[10vh] flex justify-between items-center '>
             {/* Logos */}
             <div className='w-1/2 '>
@@ -103,7 +40,7 @@ const toggleProductsDropdown = () => setProductsDropdownOpen(!productsDropdownOp
                 </div>
               )}
             </li>
-            <li onMouseEnter={toggleProductsDropdown} onMouseLeave={toggleProductsDropdown}>
+            <li >
               <a href="" className='hover:text-[#C4A66B]'>Contacto</a>
             </li>
                   </ul>
