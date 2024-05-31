b9ce24c74dcb26e8b3aecb12cfc131e724c22368
Mateo Sabelli <msabelli@uade.edu.ar>
2024-05-31 12:25:39 -0300

    Ultimos arreglos

--- src/components/services/Cards.jsx
+++ src/components/services/Cards.jsx
@@ -4,8 +4,8 @@ const Cards = (title) => {
     return (
         <div className='flex flex-col justify-center items-center bg-[#333333] w-[450px] h-[280px] rounded-3xl pt-[6px] px-[28px] pb-0 max-[993px]:w-[400px] max-[360px]:w-[250px]' >
             <div className='text-start'>
-                <h6 className='text-[#C4A66B] mb-7 text-xl max-[993px]:text-lg'>{title.title}</h6>
-                <p className='text-[#FFFFFF] text-base w-full mb-7 max-[993px]:text-sm'>
+                <h6 className='text-[#C4A66B] mb-4 text-xl max-[993px]:text-lg'>{title.title}</h6>
+                <p className='text-[#FFFFFF] text-base w-full mb-7 max-[993px]:text-sm opacity-70'>
                     {title.desc}
                 </p>
             </div>
