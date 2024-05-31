eba4a72b9c348cb53a3a339ad4e5bae2833e6f35
Mateo Sabelli <msabelli@uade.edu.ar>
2024-05-31 12:28:03 -0300

    Arreglo info

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -4,18 +4,18 @@ import FotoPerfil from "../../assets/img/Foto.png";
 
 const Info = () => {
   return (
-    <div className="py-28 max-md:py-20 ">
-      <div className="container h-auto d-flex items-center justify-center max-md:flex-col max-md:px-10 ">
-          <div className=" flex justify-start items-center gap-3 h-auto w-[50%] max-md:w-full">
+    <div className="py-28 max-lg:py-20 ">
+      <div className="container h-auto d-flex items-center justify-center max-lg:flex-col max-lg:px-10 ">
+          <div className=" flex justify-start items-center gap-3 h-auto w-[50%] max-lg:w-full">
             <img src={Logochiquito} alt="" className="h-[70px] " />
             <h2 className="text-[26px] w-[75%] pl-6 ">
               Estudio Juridico Palomeque & Asociados
             </h2>
           </div>
-          <div className="h-[20vh] w-[2.5px] mr-20 bg-black"/>
+          <div className="h-[20vh] w-[2.5px] mr-20 bg-black max-lg:hidden"/>
 
           
-        <div className="w-[50%] max-md:w-full max-md:pt-16">
+        <div className="w-[50%] max-lg:w-full max-lg:pt-16">
           <h2 className="text-2xl font-black">Somos</h2>
           <p className=" py-10 text-[20px] font-light ">
             Estudio Jurídico especializado en Prevención del Lavado de Activos y
